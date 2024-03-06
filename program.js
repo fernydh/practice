c67f86ba675a930b90bc886d007e3d6b02c2c6de
fernydh <162190780+fernydh@users.noreply.github.com>
2024-03-06 13:42:36 -0700

    Updated program.js to also output a bad color from a list of bad colors

--- program.js
+++ program.js
@@ -1,4 +1,8 @@
-colors = [red, blue, black]
+favColors = ['red', 'blue', 'black'];
+badColors = ['yellow', 'neon green'];
 
-let randomColor = Math.floor(Math.random() * colors.lengh)
-console.log(colors[randomColor]) 
+let random_favColor = Math.floor(Math.random() * favColors.length);
+console.log(favColors[random_favColor]);
+
+let random_badColor =  Math.floor(Math.random() * badColors.length);
+console.log(badColors[random_badColor]);
